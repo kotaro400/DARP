b9e1443ccb783042a8ce5410257e13a923f1c97d
TakedaKiyoshi <takedakiyoshi@TakedaKiyoshinoMacBook-Pro.local>
2019-12-09 16:22:10 +0900

    converterを修正 /data以下のファイルを全て変形して/revisedにいれれるようにした

--- converter.py
+++ converter.py
@@ -1,22 +1,28 @@
 import re
+import os
 path = 'instance.txt'
 path_w = 'output.txt'
-line = []
-with open(path) as f:
-    for s_line in f:
-        words = re.split(" +", s_line)
-        line.append(words)
-print(line)
-print(line[2][0])
-with open(path_w, mode='a') as f:
-    for item in line:
-        for subitem in item:
-            if  subitem.find(' ') == -1:
-                if subitem == "":
-                    pass
-                elif subitem.find('\n') == -1:
-                    str = subitem + ","
-                    f.write(str)
-                else:
-                    f.write(subitem)
-                    print('aaa')
+filepath = './data'
+files = os.listdir(filepath)
+for file in files:
+    if file != ".DS_Store":
+        path = "./data/" + file
+        line = []
+        with open(path) as f:
+            for s_line in f:
+                words = re.split(" +", s_line)
+                line.append(words)
+        print(line)
+        outputpath = "./revised/" + file
+        outputpath = os.path.abspath(outputpath)
+        with open(outputpath, mode='a') as f:
+            for item in line:
+                for subitem in item:
+                    if  subitem.find(' ') == -1:
+                        if subitem == "":
+                            pass
+                        elif subitem.find('\n') == -1:
+                            str = subitem + ","
+                            f.write(str)
+                        else:
+                            f.write(subitem)
