a82835d3ef4087116e7d98f431dca36873975d15
TakedaKiyoshi <takedakiyoshi@TakedaKiyoshinoMacBook-Pro.local>
2019-12-12 15:45:45 +0900

    converterの修正 傾き5と-5の関数をリクエストの次の行に出力

--- converter.py
+++ converter.py
@@ -12,17 +12,36 @@ for file in files:
             for s_line in f:
                 words = re.split(" +", s_line)
                 line.append(words)
-        print(line)
+        # print(line)
+        MaximuRouteDuraion = int(line[0][2])
         outputpath = "./revised/" + file
         outputpath = os.path.abspath(outputpath)
-        with open(outputpath, mode='a') as f:
+        with open(outputpath, mode='w') as f:
+            for subitem in line[0]:
+                if  subitem.find(' ') == -1:
+                        if subitem == "":
+                            pass
+                        elif subitem.find('\n') == -1:
+                            stri = subitem + ","
+                            f.write(stri)
+                        else:
+                            f.write(subitem)
+            line.pop(0)
             for item in line:
                 for subitem in item:
                     if  subitem.find(' ') == -1:
                         if subitem == "":
                             pass
                         elif subitem.find('\n') == -1:
-                            str = subitem + ","
-                            f.write(str)
+                            stri = subitem + ","
+                            f.write(stri)
                         else:
                             f.write(subitem)
+                if (int(item[-1].strip("\n")) >= MaximuRouteDuraion) and (int(item[-2]) == 0):
+                    f.write("0,0,0"+"\n")
+                elif int(item[-1].strip("\n")) >= MaximuRouteDuraion:
+                    f.write("0,-5,"+str(int(item[-2])*5)+","+item[-2]+",0,0\n")
+                elif int(item[-2]) == 0:
+                    f.write("0,0,0,"+item[-1]+",5,"+str(int(item[-1].strip("\n"))*-5)+"\n")
+                else:
+                    f.write("0,-5,"+str(int(item[-2])*5)+","+item[-2]+",0,0,"+item[-1].strip("\n")+",5,"+str(int(item[-1].strip("\n"))*-5)+"\n")
