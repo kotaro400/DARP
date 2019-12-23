24b1331cb3d9ed21641f94f609b080fb5ac97766
TakeKiyo <d8sk4ueum@gmail.com>
2019-12-23 17:53:40 +0900

    10回ループさせてみた ちゃんとランダムに変わってそう

--- handle_vector.cpp
+++ handle_vector.cpp
@@ -7,6 +7,7 @@ using namespace std;
 void removeElement(vector<int> &vector, int index);
 int main(){
     vector<int> vec;
+    int random_int,first,second;
     for(int i=0;i<9;i++){
         vec.push_back(i);
     }
@@ -15,45 +16,40 @@ int main(){
         std::cout << vec[i] << " ";
     }
     cout << endl;
-    cout << "vec:" << vec.capacity() << endl;
 
     mt19937_64 mt64(0);
 
-    int random_int = mt64();
-    cout << abs(random_int)%4 << endl;
+    for(int i=0;i<10;i++){
+    random_int = mt64();
 
-    int first = vec.at(1+2*(abs(random_int)%4));
+    first = vec.at(1+2*(abs(random_int)%4));
+    second = vec.at(1+2*(abs(random_int)%4)+1);
     removeElement(vec,1+2*(abs(random_int)%4));
-    int second = vec.at(1+2*(abs(random_int)%4));
     removeElement(vec,1+2*(abs(random_int)%4));
 
-    cout << "削除されたのは," << first << " " << second << endl;
-    cout << "-------" << endl;
-    for(int i=0;i<vec.size();i++){
-        std::cout << vec[i] << " ";
-    }
-    cout << endl;
-    cout << "削除後のvec:" << vec.capacity() << endl;
+    // cout << "削除されたのは," << first << " " << second << endl;
+    // for(int i=0;i<vec.size();i++){
+    //     std::cout << vec[i] << " ";
+    // }
+    // cout << endl;
+    // cout << "削除後のvec:" << vec.capacity() << endl;
 
 
     // 挿入
     random_int = mt64();
-    cout << abs(random_int)%4 << endl;
     auto it = vec.begin();
     it += 1+2*(abs(random_int)%4);
-    cout << *it << endl;
     it = vec.insert(it, first);
     it++;
     it = vec.insert(it, second);
     it++;
 
-    cout << "------" << endl;
     for(int i=0;i<vec.size();i++){
         std::cout << vec[i] << " ";
     }
     cout << endl;
+    }
 
-    cout << "挿入後のvec:" << vec.capacity() << endl;
 
 }
 
