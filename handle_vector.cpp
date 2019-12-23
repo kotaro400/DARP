ebe3cc2bd0df258500162fe9288637e38532d945
TakeKiyo <d8sk4ueum@gmail.com>
2019-12-23 17:38:16 +0900

    vectorのerase,insertをhandle_vectorでテスト

---
+++ handle_vector.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+int main(){
+    vector<int> vec;
+    for(int i=0;i<9;i++){
+        vec.push_back(i);
+    }
+    vec.push_back(0);
+    for(int i=0;i<10;i++){
+        std::cout << vec[i] << " ";
+    }
+    cout << endl;
+    cout << "vec:" << vec.capacity() << endl;
+
+    mt19937_64 mt64(0);
+
+    int random_int = mt64();
+    cout << abs(random_int)%4 << " ";
+    cout << vec.at(1+2*(abs(random_int)%4)) << endl;
+    int first = vec.at(1+2*(abs(random_int)%4));
+    vec.erase(vec.begin()+(1+2*(abs(random_int)%4)));
+    int second = vec.at(1+2*(abs(random_int)%4));
+
+    vec.erase(vec.begin()+(1+2*(abs(random_int)%4)));
+    cout << "削除されたのは," << first << " " << second << endl;
+    cout << "-------" << endl;
+    for(int i=0;i<vec.size();i++){
+        std::cout << vec[i] << " ";
+    }
+    cout << endl;
+    cout << "削除後のvec:" << vec.capacity() << endl;
+
+    // 挿入
+    random_int = mt64();
+    cout << abs(random_int)%4 << endl;
+    auto it = vec.begin();
+    it += 1+2*(abs(random_int)%4);
+    cout << *it << endl;
+    it = vec.insert(it, first);
+    it++;
+    it = vec.insert(it, second);
+    it++;
+
+    cout << "------" << endl;
+    for(int i=0;i<vec.size();i++){
+        std::cout << vec[i] << " ";
+    }
+    cout << endl;
+
+    cout << "挿入後のvec:" << vec.capacity() << endl;
+
+}
